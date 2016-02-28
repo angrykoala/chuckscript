fb84c961061c0b2a318be93548cc43edb6b7a65a
demiurgosoft <demiurgosoft@hotmail.com>
2016-02-28 22:01:57 +0100

    regexp to get code, now comments can be placed

--- cni.js
+++ cni.js
@@ -9,15 +9,20 @@ Usage: node cni.js myprogram.cnpl
 var bigInt = require('big-integer');
 var fs = require('fs');
 var process = require('process');
-
 var ABC = require('./lib/abc.js');
 
+var regex=/\[0\]{\d+}/;
+
+
 var cnCode = process.argv[2];
-if (!cnCode) return console.log("Usage node cni.js program.cnpl");
+if (!cnCode) return console.log("Usage: node cni.js program.cnpl");
 fs.readFile(cnCode, 'utf8', function(err, data) {
 	if (err) {
 		return console.log(err);
 	}
+	console.log(data);
+	data=data.match(regex)[0];
+	console.log(data);
 
 	var value = data.slice(4, -1);
 	var num = bigInt(value);
